5b51aa0dd8d7e07e069c5644c0606f21c3f7c2d3
Curtis Brinker <curtisbrnkr@gmail.com>
2023-02-17 17:32:28 -0600

    Add tanh

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch.nn as nn
 from torch import tensor
 from mlcvlab.nn import activations
+from typing import Union
 
 
 
@@ -12,7 +13,7 @@ def test_relu():
         torch_func=nn.ReLU,
         mlcv_z_func=activations.relu,
         mlcv_grad_func=activations.relu_grad,
-        n=10000
+        size=10000
     )
 
 def test_sigmoid():
@@ -21,7 +22,21 @@ def test_sigmoid():
         torch_func=nn.Sigmoid,
         mlcv_z_func=activations.sigmoid,
         mlcv_grad_func=activations.sigmoid_grad,
-        n=10000
+        size=10000
+    )
+
+
+def test_softmax():
+    pass
+
+
+def test_tanh():
+    """Test the output and gradient of the mlcvlab.nn.activations.tanh function"""
+    _run_activation_test(
+        torch_func=nn.Tanh,
+        mlcv_z_func=activations.tanh,
+        mlcv_grad_func=activations.tanh_grad,
+        size=10000
     )
 
 
@@ -29,7 +44,7 @@ def _run_activation_test(
     torch_func: nn.Module, 
     mlcv_z_func: callable, 
     mlcv_grad_func: callable, 
-    n: int
+    size: Union[int, tuple[int, ...]]
 ) -> None:
     """Computes and compares the results of an activation function and the associated gradient
 
@@ -37,9 +52,12 @@ def _run_activation_test(
         torch_func (nn.Module): PyTorch activation function module
         mlcv_z_func (callable): Activation function
         mlcv_grad_func (callable): Activation gradient function
-        n (int): Size of random vector to test
+        size (Union[int, tuple[int, ...]]): Size of random vector to test, 
+            if int is passed shape is changed to (shape, 1)
     """
-    X = np.random.uniform(-10, 10, size=(n, 1))
+    if isinstance(size, int):
+        size = (size, 1)
+    X = np.random.uniform(-10, 10, size=size)
     torch_z, torch_grad = _z_grad_ground_truth(X, torch_func())
     user_z = mlcv_z_func(X)
     user_grad = mlcv_grad_func(user_z)
